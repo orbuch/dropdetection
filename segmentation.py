baa2cb1f783d01d23bc9ce9d9857b09c987248fa
Ryan Orbuch <ryan@joinvolley.com>
2017-07-30 04:02:47 -0700

    add local files

---
+++ segmentation.py
@@ -0,0 +1,20 @@
+import numpy as np
+from hpcp import hpcp
+
+def get_past_comparate():
+    pass
+
+def compare_segments():
+    pass
+
+def featurize():
+    pass
+
+def calculate_novelty_curve():
+    pass
+
+def main():
+    test = hpcp('data/liam.wav', output='numpy')
+    # print(test)
+main()
+    
